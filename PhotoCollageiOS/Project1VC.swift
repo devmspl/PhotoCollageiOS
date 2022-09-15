53d2b846a85ecd24fadfd210fe028c449740ee26
devMspl <developer.mspl@gmail.com>
2022-09-15 10:42:46 +0530

    latestCode

---
+++ PhotoCollageiOS/Project1VC.swift
@@ -0,0 +1,29 @@
+//
+//  Project1VC.swift
+//  PhotoCollageiOS
+//
+//  Created by MacBook M1 on 14/09/22.
+//
+
+import UIKit
+
+class Project1VC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
